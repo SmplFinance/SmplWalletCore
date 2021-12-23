269aab426f4e6542bd91d079e0e16da28c84711c
Jacob Boswell <jacob@s56.net>
2021-12-23 13:50:09 -0700

    got keycloak auth working

--- projects/application-showcase/src/app/app.module.ts
+++ projects/application-showcase/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SmplWalletCoreModule} from '@smpl/smpl-wallet-core';
+import { WalletComponent } from './components/wallet/wallet.component';
+import { OpenComponent } from './components/open/open.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WalletComponent,
+    OpenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SmplWalletCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
